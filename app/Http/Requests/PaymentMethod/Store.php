4b71f7bb4c3a9cb435420021993f40d78a891f2e
Muhammad Shoaib <mshoaibdev@gmail.com>
2024-01-08 12:43:02 +0500

    Refactor payment method validation rules

--- app/Http/Requests/PaymentMethod/Store.php
+++ app/Http/Requests/PaymentMethod/Store.php
@@ -22,12 +22,12 @@ class Store extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255', 'unique:payment_methods,name,NULL,id,company_id,' . $this->header('company')],
+            'name' => ['required', 'string', 'max:255'],
             'is_default' => ['required', 'boolean'],
             'is_enabled' => ['required', 'boolean'],
             'is_gateway' => ['required', 'boolean'],
-            'live_identifier' => ['required_if:is_gateway,true', 'nullable',  'max:255'],
-            'live_secret' => ['required_if:is_gateway,true',  'max:255'],
+            'live_identifier' => ['required_if:mode,live', 'max:255'],
+            'live_secret' => ['required_if:mode,live','max:255'],
             'sandbox_identifier' => ['required_if:mode,sandbox',  'max:255'],
             'sandbox_secret' => ['required_if:mode,sandbox',  'max:255'],
             'mode' => ['required', 'string', 'max:255'],
--- app/Http/Requests/PaymentMethod/Update.php
+++ app/Http/Requests/PaymentMethod/Update.php
@@ -24,12 +24,12 @@ class Update extends FormRequest
     {
         
         return [
-            'name' => ['required', 'string', 'max:255', Rule::unique('payment_methods')->where('company_id', $this->header('company'))->ignore($this->route('payment_method')->id)],
+            'name' => ['required', 'string', 'max:255'],
             'is_default' => ['required', 'boolean'],
             'is_enabled' => ['required', 'boolean'],
             'is_gateway' => ['required', 'boolean'],
-            'live_identifier' => ['required_if:is_gateway,true', 'nullable', 'max:255'],
-            'live_secret' => ['required_if:is_gateway,true', 'max:255'],
+            'live_identifier' => ['required_if:mode,live', 'max:255'],
+            'live_secret' => ['required_if:mode,live', 'max:255'],
             'sandbox_identifier' => ['required_if:mode,sandbox', 'max:255'],
             'sandbox_secret' => ['required_if:mode,sandbox', 'max:255'],
             'mode' => ['required', 'string', 'max:255'],
